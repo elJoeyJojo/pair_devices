e616746823a787c3d1640d999bf18b1b10b13c08
elJoeyJojo <hookups013@hotmail.com>
2020-09-24 13:28:09 -0700

    updating the grep command

--- pair_devices.sh
+++ pair_devices.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mac_add=$(grep $1 device_temp.txt | awk '{print $1}')
+mac_add=$(grep -m 1 $1 device_temp.txt | awk '{print $1}')
 sudo sed -i '/\['$2'\]/,/ble/ s/ble_mac=/ble_mac='$mac_add'/' /etc/gw.conf
 sudo sed -i '\/['$3'\]/,/active/ s/active_devices=/active_devices='$3'/' /etc/gw.conf
